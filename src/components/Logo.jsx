23ab01064c2202d21b2abba2fc7435f47343d8ff
JayGangwar07 <jaygangwar86@gmail.com>
2025-02-27 21:13:35 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px',styling}) {
 return (
 <img 
-src="../IMG_20250227_210506-removebg-preview.png"
+src="../../IMG_20250227_210506-removebg-preview.png"
 width={width}
 className={`animate-spin`styling}
 />
